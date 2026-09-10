01ee6f3a99c80503d4f956f6ba8403547b70960d
Omar Shrit <omar@shrit.me>
2026-09-10 22:13:10 +0200

    Add the implementation of zlib based decompression
    Signed-off-by: Omar Shrit <omar@shrit.me>

---
+++ src/mlpack/core/data/decompress_gz.hpp
@@ -0,0 +1,62 @@
+/**
+ * @file core/data/decompress_gz.hpp
+ * @author Omar Shrit
+ *
+ * Standalone gzip decompression using zlib.
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
+#ifndef MLPACK_CORE_DATA_DECOMPRESS_GZ_HPP
+#define MLPACK_CORE_DATA_DECOMPRESS_GZ_HPP
+
+#include <mlpack/prereqs.hpp>
+
+namespace mlpack {
+
+/**
+ * Check if a file contains gzip-compressed data by inspecting the first two
+ * bytes for the gzip magic number (0x1F 0x8B).
+ *
+ * @param filePath Path to the file to check.
+ * @return true if the file starts with the gzip magic bytes.
+ */
+inline bool IsGzipFile(const std::string& filePath);
+
+#ifdef MLPACK_USE_ZLIB
+
+/**
+ * Decompress a gzip file and write the result to a new file.  Uses zlib's
+ * gzopen/gzread API directly.
+ *
+ * @param srcPath Path to the gzip-compressed file.
+ * @param destPath Path where the decompressed file will be written.
+ * @return true on success, throws on failure.
+ */
+inline bool DecompressGzFile(const std::string& srcPath,
+                             const std::string& destPath);
+
+#endif // MLPACK_USE_ZLIB
+
+/**
+ * If `filename` has a .gz extension, decompress it to a temporary file and
+ * update `filename` to point to the decompressed result.  The original .gz
+ * extension is stripped and replaced with the underlying extension (e.g.
+ * "data.csv.gz" produces a temp file ending in ".csv").
+ *
+ * If the file is not gzip-compressed (no .gz extension, or the magic bytes
+ * don't match), `filename` is left unchanged.
+ *
+ * @param filename On input, path to the (possibly compressed) file.  On
+ *     output, path to the decompressed file (or unchanged if no decompression
+ *     was needed).
+ */
+inline void DecompressGzipIfNeeded(std::string& filename);
+
+} // namespace mlpack
+
+#include "decompress_gz_impl.hpp"
+
+#endif
---
+++ src/mlpack/core/data/decompress_gz_impl.hpp
@@ -0,0 +1,119 @@
+/**
+ * @file core/data/decompress_gz_impl.hpp
+ * @author Omar Shrit
+ *
+ * Implementation of standalone gzip decompression using zlib.
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
+#ifndef MLPACK_CORE_DATA_DECOMPRESS_GZ_IMPL_HPP
+#define MLPACK_CORE_DATA_DECOMPRESS_GZ_IMPL_HPP
+
+#include "decompress_gz.hpp"
+#include "extension.hpp"
+#include "handle_files.hpp"
+
+#ifdef MLPACK_USE_ZLIB
+  #include <zlib.h>
+#endif
+
+namespace mlpack {
+
+inline bool IsGzipFile(const std::string& filePath)
+{
+  std::ifstream f(filePath, std::ios::binary);
+  if (!f)
+    return false;
+
+  unsigned char magic[2] = {0, 0};
+  f.read(reinterpret_cast<char*>(magic), 2);
+
+  return f.good() && magic[0] == 0x1F && magic[1] == 0x8B;
+}
+
+#ifdef MLPACK_USE_ZLIB
+
+inline bool DecompressGzFile(const std::string& srcPath,
+                             const std::string& destPath)
+{
+  gzFile gzIn = gzopen(srcPath.c_str(), "rb");
+  if (!gzIn)
+  {
+    throw std::runtime_error("DecompressGzFile(): cannot open '" + srcPath
+        + "' for reading.");
+  }
+
+  std::ofstream out(destPath, std::ios::binary);
+  if (!out)
+  {
+    gzclose(gzIn);
+    throw std::runtime_error("DecompressGzFile(): cannot open '" + destPath
+        + "' for writing.");
+  }
+
+  constexpr size_t bufSize = 131072; // 128 KB buffer.
+  std::vector<char> buf(bufSize);
+
+  int bytesRead = 0;
+  while ((bytesRead = gzread(gzIn, buf.data(), bufSize)) > 0)
+  {
+    out.write(buf.data(), bytesRead);
+    if (!out.good())
+    {
+      gzclose(gzIn);
+      throw std::runtime_error("DecompressGzFile(): error writing to '"
+          + destPath + "'.");
+    }
+  }
+
+  if (bytesRead < 0)
+  {
+    int errnum = 0;
+    const char* errMsg = gzerror(gzIn, &errnum);
+    std::string msg(errMsg ? errMsg : "unknown error");
+    gzclose(gzIn);
+    throw std::runtime_error("DecompressGzFile(): zlib error while "
+        "decompressing '" + srcPath + "': " + msg);
+  }
+
+  gzclose(gzIn);
+  return true;
+}
+
+#endif // MLPACK_USE_ZLIB
+
+inline void DecompressGzipIfNeeded(std::string& filename)
+{
+  std::string ext = Extension(filename);
+  if (ext != "gz")
+    return;
+
+#ifdef MLPACK_USE_ZLIB
+  if (!IsGzipFile(filename))
+    return;
+
+  // Extract the underlying extension in order to get the temp file name.
+  std::string innerName = filename.substr(0, filename.size() - 3);
+  std::string innerExt = Extension(innerName);
+
+  std::filesystem::path dest = TempName();
+  if (!innerExt.empty())
+    dest += "." + innerExt;
+
+  DecompressGzFile(filename, dest.string());
+  filename = dest.string();
+
+#else
+  throw std::runtime_error("Load(): the file '" + filename + "' is "
+      "gzip-compressed but MLPACK_USE_ZLIB is not enabled.  Enable zlib "
+      "support by adding '#define MLPACK_USE_ZLIB' before including mlpack "
+      "and linking with -lz, or pass -DENABLE_ZLIB=ON to CMake.");
+#endif
+}
+
+} // namespace mlpack
+
+#endif
